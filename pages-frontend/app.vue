b6fa684f8805fdbcc1211cb8fd6167fa7a431489
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2024-01-12 14:31:20 +0100

    feature(pages): add pages source code

---
+++ pages-frontend/app.vue
@@ -0,0 +1,7 @@
+<template>
+    <NuxtLayout>
+        <main>
+            <NuxtPage />
+        </main>
+    </NuxtLayout>
+</template>
---
+++ pages-frontend/components/ProjectImageBanner.vue
@@ -0,0 +1,50 @@
+<!--
+/**
+ * File Name: ProjectImageBanner.vue
+ * Author: Alexandre Kévin DE FREITAS MARTINS
+ * Creation Date: 2024
+ * Description: ProjectImageBanner.vue
+ * Company: Tux Inc.
+ * Version: 1.0.0
+ * License: MIT
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the 'Software'), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+-->
+
+<script setup lang="ts">
+defineProps({
+    img: {
+        type: String,
+        default: "/img/img.jpg",
+    },
+});
+</script>
+
+<template>
+    <div>
+        <div class="flex justify-center">
+            <img
+                class="rounded-lg shadow-2xl my-2 justify-center"
+                :src="`/img/${img}`"
+                alt="${img}"
+            />
+        </div>
+    </div>
+</template>
