1516fcbf90b693b32dca70073442253ad90e3ce1
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2026-01-01 20:52:22 +0100

    chores: remove doc folder -> old things

--- docs/_nuxt/DocumentDrivenNotFound.p9j5ZR_O.js
+++
@@ -1,1 +0,0 @@
-import{f as n,l as e}from"./entry.ab7fK3w4.js";const t=n({name:"DocumentDrivenNotFound",render(){return e("div","Document not found")}});export{t as default};
--- docs/_nuxt/ProseCode.n0DZcSPz.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./ProseCode.vue.wdQnacH_.js";import"./entry.ab7fK3w4.js";export{m as default};
